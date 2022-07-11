1820ca5a395a3edc4b5de3e6c72ff43c2cd6119a
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2022-07-11 04:25:57 +0200

    CI Build Matrix with Java 8, 11 and 17 (#14)
    Closes #11

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     id("dev.lajoscseppento.ruthless") version "0.4.0"
+    id("com.gradle.enterprise") version "+"
 }
 
 rootProject.name = "fancydoc"
 include("fancydoc-plugin")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
